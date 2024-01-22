4541cc17e73cd932057d9bb6e5994f98980d42cd
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2024-01-22 18:25:54 +0200

    skeleton card component and view

---
+++ src/Card.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	let { name, handleYes, handleNo } = $props();
+</script>
+
+<div>
+	{name}
+
+	<button onclick={() => dispatch('result', true)}>Yes</button>
+	<button onclick={() => dispatch('result', false)}>No</button>
+</div>
+
+<style>
+	div {
+		border: solid black;
+		display: inline-block;
+		padding: 1rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Card from '../Card.svelte';
+	let remaining = $state(['Tami', 'Timo', 'Simo', 'Tomi', 'Sami', 'Somo', 'Timi']);
+
+	const current = $derived(remaining[0]);
+
+	let voted = $state([]);
+
+	function handleResult(event) {
+		voted = [...voted, { name: current, vote: event.detail }];
+		remaining = remaining.slice(1);
+	}
+</script>
+
+{#if remaining.length}
+	<div>
+		<Card name={current} on:result={handleResult} />
+	</div>
+{:else}
+	All done!
+{/if}
+
+<h2>Voted</h2>
+
+{#each voted as { name, vote }}
+	<p>Voted {vote ? 'Yes' : 'No'} for {name}</p>
+{/each}
