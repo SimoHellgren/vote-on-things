46947b5e7051e57a1c6e03aa1fbe07310ca8f006
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2024-02-05 20:21:08 +0200

    fix auth

--- src/routes/lists/[id]/vote/+page.server.js
+++ src/routes/lists/[id]/vote/+page.server.js
@@ -1,9 +1,6 @@
-import { supabase } from "$lib/supabaseClient";
-
-export async function load({ params }) {
+export async function load({ params, locals: { supabase } }) {
     const { data: items } = await supabase.from("item").select().eq('list_id', params.id);
     const { data: list } = await supabase.from("list").select().eq('id', params.id).single();
-
 
     return {
         list,
